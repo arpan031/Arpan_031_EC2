0c63dab8b205f5b458a74f7ba7ddf7b8951cfd84
arpan031 <arpanghosh4617@gmail.com>
2025-03-31 01:49:09 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Arpan Wlcome to EC2')
+  res.send('Hello MCKV')
 })
 
 app.get('/courses', function (req, res) {
